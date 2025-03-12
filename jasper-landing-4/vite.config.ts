49b9cfc960d379931e136186e8e10e7e878a547a
Olanrewaju Shinaayomi <nexatechx@gmail.com>
2025-03-12 17:09:47 +0100

    Update vite.config.ts

--- jasper-landing-4/vite.config.ts
+++ jasper-landing-4/vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-     base: '/jasper-landing-4',
     react(),
     mode === 'development' &&
     componentTagger(),
