a02cf7f66c5599ff9995f5f329be9eba54e01910
Olanrewaju Shinaayomi <nexatechx@gmail.com>
2025-03-12 17:08:20 +0100

    Update vite.config.ts

--- Jasper Landing 1/vite.config.ts
+++ Jasper Landing 1/vite.config.ts
@@ -4,7 +4,6 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jasper landing 1',
   plugins: [
     react(),
     ViteImageOptimizer({
